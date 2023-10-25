92b05605f08a548050c022990f6562cbeab2a4fa
Olga <Taylakova_Olga@mail.ru>
2023-10-25 20:32:48 +0700

    first commit

---
+++ src/main/java/ru/netology/Task.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import java.util.Objects;
+
+public abstract class Task {
+    protected int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public abstract boolean matches(String query);
+}
